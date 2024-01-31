8e5a8b92149ea7083b57b6c103e602412b486c12
Augusto Assis <au.assis@reply.com>
2024-01-31 09:50:20 -0300

    first commit

---
+++ web/src/app/pages/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  ;
+  public isLoading: boolean;
+  public successLogin: boolean;
+  public loginForm: FormGroup
+  
+  constructor(private router: Router, private fb: FormBuilder)
+  { 
+    this.loginForm = this.fb.group({
+      login: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      password:['', Validators.required]
+    })
+
+    this.isLoading = false;
+    this.successLogin = true;
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid)
+      this.router.navigate([''])
+  }
+}
